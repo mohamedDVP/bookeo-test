a9628516cfbfdd19de266c4ce7c6c97e940ea33d
mohamedDVP <mohamed.mak@laposte.net>
2024-02-24 15:45:25 +0100

    update config file - db_name

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
     return [
-        'db_name' => 'studi_bookeo',
+        'db_name' => 'bookeo',
         'db_user' => 'root',
-        'db_password' => '',
+        'db_password' => 'YMYErdzvp)TxDjX/',
         'db_port' => '3306',
         'db_host' => 'localhost'
     ];
